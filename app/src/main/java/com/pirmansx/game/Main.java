34d9cb8f3dc5d7c0bdca3244cf11f4b2f569f6f2
pirmansx <adifirmans000@gmail.com>
2024-06-08 02:30:40 +0700

    update

--- app/src/main/java/com/pirmansx/game/Main.java
+++ app/src/main/java/com/pirmansx/game/Main.java
@@ -1,18 +1,8 @@
 package com.pirmansx.game;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 
 public class Main extends android.app.NativeActivity {
     static {
-        
         System.loadLibrary("imgui");
         System.loadLibrary("game");
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        
-        
     }
 }
